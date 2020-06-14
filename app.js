75fb476626df4a95b558dd1f8ad6e0dc5618614f
Anitha Venkatesan <anithamca68@gmail.com>
2020-06-14 09:16:35 -0500

    Added delete notes and responding post with json

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Dependencies
 let express = require("express");
 let path = require("path");
-let fs= require("fs");
+let fs = require("fs");
+let lodash = require("lodash");
 
 // Setting  up the Express App
 var app = express();
@@ -13,30 +14,41 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 //Creating an API routes 
-app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "./db/db.json"));
-  console.log("Getting the Json file");  
+  console.log("Getting the Json file");
 });
+
 //Creating an html routes
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 // Create New notes - takes in JSON input
-app.post("/api/notes", function(req, res) {
-  const dbJson = fs.readFileSync('./db/db.json', 'utf-8'); 
+app.post("/api/notes", function (req, res) {
+  const dbJson = fs.readFileSync('./db/db.json', 'utf-8');
   const jsonData = JSON.parse(dbJson);
-  req.body.id = jsonData.length;
+  req.body.id = jsonData.length + 1;
   jsonData.push(req.body);
   fs.writeFileSync("./db/db.json", JSON.stringify(jsonData));
+  res.json(req.body);
+});
+//Deleting a notes using unique id
+app.delete("/api/notes/:id", function (req, res) {
+  const dbJson = fs.readFileSync('./db/db.json', 'utf-8');
+  const jsonData = JSON.parse(dbJson);
+  let id = req.params.id;
+  console.log(id);
+  const newJsonData = lodash.reject(jsonData, {'id': Number(id) });
+  console.log("newJsonData");
+  console.log(newJsonData);
+  fs.writeFileSync("./db/db.json", JSON.stringify(newJsonData));
   res.end();
 });
-
-app.get("/", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-
 // Starts the server to begin listening
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
